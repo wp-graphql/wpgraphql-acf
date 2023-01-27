fa53cf57113b412663f215383e11fd528f1a5f18
Mark Kelnar <749603+markkelnar@users.noreply.github.com>
2023-01-27 11:39:25 -0600

    Functional test for custom fields admin settings page

---
+++ tests/functional/AdminSettingsCest.php
@@ -0,0 +1,44 @@
+<?php
+
+class AdminSettingsCest
+{
+
+    public function _before( FunctionalTester $I ) {
+        $json_file = 'acf-export-2023-01-26.json';
+        $I->importJson( $json_file );
+    }
+
+    public function seeCustomFieldsFieldGroupWpgraphqlHeadersTest( FunctionalTester $I ) {
+        $I->loginAsAdmin();
+
+        $I->amOnPage('/wp-admin/edit.php?post_type=acf-field-group');
+
+        // The graphql headings
+        $I->see('Graphql Type', "//thead/tr/th[@id='acf-wpgraphql-type']");
+        $I->see('Graphql Interfaces', "//thead/tr/th[@id='acf-wpgraphql-interfaces']");
+        $I->see('Graphql Locations', "//thead/tr/th[@id='acf-wpgraphql-locations']");
+
+    }
+
+    public function seeCustomFieldsFieldGroupTableTest( FunctionalTester $I ) {
+
+        // Import the json file Save and see the selection after form submit
+        $I->loginAsAdmin();
+        // $json_file = 'acf-export-2023-01-26.json';
+        // $I->amOnPage('/wp-admin/edit.php?post_type=acf-field-group&page=acf-tools');
+        // $I->attachFile('//*[@id="acf_import_file"]', $json_file );
+        // $I->submitForm('//*[@id="acf-admin-tool-import"]/div[2]/form/div[2]/p/input', [], 'Choose File');
+
+        // On field group page, verify see fields
+        $I->amOnPage('/wp-admin/edit.php?post_type=acf-field-group');
+
+        $r = $I->grabTextFrom( "//tbody/tr/td[1]/*/a[@class='row-title']" );
+        codecept_debug( $r );
+
+        // Grab the first field group name
+        $I->assertEquals( 'Foo Name', $I->grabTextFrom( "//tbody/tr/td[1]/*/a[@class='row-title']" ) );
+
+        // Grab the wpgraphql type name
+        $I->assertEquals( 'FooGraphql', $I->grabTextFrom( "//tbody/tr/td/span[@class='acf-wpgraphql-type']" ) );
+    }
+}
