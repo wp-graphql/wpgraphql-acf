af27cada79792b04035ed94a531910344e699e44
Mark Kelnar <749603+markkelnar@users.noreply.github.com>
2023-01-27 11:40:34 -0600

    acf import file for test field group admin page

---
+++ tests/_support/Helper/Utils.php
@@ -0,0 +1,20 @@
+<?php
+namespace Helper;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class Utils extends \Codeception\Module
+{
+    public function importJson( $json_file ) {
+
+        // Import the json file Save and see the selection after form submit
+        $this->getModule('WPBrowser')->loginAsAdmin();
+
+        $this->getModule('WPBrowser')->amOnPage('/wp-admin/edit.php?post_type=acf-field-group&page=acf-tools');
+
+        $this->getModule('WPBrowser')->attachFile('//*[@id="acf_import_file"]', $json_file );
+
+        $this->getModule('WPBrowser')->submitForm('//*[@id="acf-admin-tool-import"]/div[2]/form/div[2]/p/input', [], 'Choose File');
+    }
+}
