4c1b5ede009541e936b94cb627dff78252da3a4b
Jason Bahl <jasonbahl@mac.com>
2023-01-27 13:49:54 -0700

    - add default license key and version in .env.testing.dist - modify logic for when to test against free vs. pro (when a license key is provided, download PRO)

--- docker/app.setup.sh
+++ docker/app.setup.sh
@@ -6,14 +6,14 @@
 
 PLUGINS_DIR=${PLUGINS_DIR-.}
 ACF_LICENSE_KEY=${ACF_LICENSE_KEY-.}
-ACF_VERSION=${ACF_VERSION-.}
-ACF_PRO=${ACF_PRO-.}
+ACF_VERSION=${ACF_VERSION-"latest"}
 
 # If an ACF_VERSION is passed, use it, else the latest version will be downloaded
 ACF_DOWNLOAD_VERSION=''
 
-[[ $ACF_VERSION != '' ]]
+if [[ -n ${ACF_VERSION} && "${ACF_VERSION}" != "latest" ]]; then
 	ACF_DOWNLOAD_VERSION="&t=${ACF_VERSION}"
+fi
 
 echo "Plugins dir ($PLUGINS_DIR)"
 echo "ACF_LICENSE_KEY ($ACF_LICENSE_KEY)"
@@ -35,23 +35,30 @@ fi
 # Activate the plugin
 wp plugin activate wp-graphql-acf-redux --allow-root
 
-# IF the test should run against ACF PRO
-if [ true == $ACF_PRO ]; then
+# If a license key is provided
+# use ACF Pro for the tests
+if [[ -n ${ACF_LICENSE_KEY} && '.' == ${ACF_LICENSE_KEY} || 'Your License Key' == ${ACF_LICENSE_KEY} ]]; then
 
-		# Some version of acf-pro that let's tests pass.
-		if [ ! -d ${PLUGINS_DIR}/advanced-custom-fields-pro ]; then
-				echo "Installing ACF Pro from AdvancedCustomFields.com"
-				## NOTE we can add &t=${ACF_VERSION}
-				wp plugin install "https://connect.advancedcustomfields.com/v2/plugins/download?p=pro&k=${ACF_LICENSE_KEY}${ACF_DOWNLOAD_VERSION}" --activate --allow-root
+		echo "ACF version: ${ACF_VERSION}"
 
+		if [[ -z ${ACF_VERSION} || "${ACF_VERSION}" == "latest" ]]; then
+			echo "Installing ACF FREE (latest) from wordpress.org"
+			wp plugin install advanced-custom-fields --allow-root --activate
 		else
-			echo "Warning: Advanced Custom Fields Pro plugin already installed"
+           	echo "Installing ACF FREE (v${ACF_VERSION}) from wordpress.org"
+			wp plugin install advanced-custom-fields --version=$ACF_VERSION --allow-root --activate
 		fi
 
 	else
 
-		echo "Installing ACF FREE from wordpress.org"
-		wp plugin install advanced-custom-fields --version=$ACF_VERSION --allow-root --activate
+		if [ ! -d ${PLUGINS_DIR}/advanced-custom-fields-pro ]; then
+			echo "Installing ACF Pro from AdvancedCustomFields.com"
+			## NOTE we can add &t=${ACF_VERSION}
+			wp plugin install "https://connect.advancedcustomfields.com/v2/plugins/download?p=pro&k=${ACF_LICENSE_KEY}${ACF_DOWNLOAD_VERSION}" --activate --allow-root
+
+		else
+			echo "Warning: Advanced Custom Fields Pro plugin already installed"
+		fi
 
 fi
 
