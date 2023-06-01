dece70948be904ef67e313de0d11214120378bc4
Jason Bahl <jasonbahl@mac.com>
2023-06-01 14:21:32 -0600

    - Prevent adding clone interfaces if ACF Pro is not active

--- tests/_support/WPUnit/AcfeFieldType.php
+++ tests/_support/WPUnit/AcfeFieldType.php
@@ -6,12 +6,6 @@ class AcfeFieldType extends \acf_field  {
 	public function __construct( $name ) {
 		$this->name = $name;
 		$this->label = $name;
-
-		// if ACF PRO is not active, skip the test
-		if ( ! defined( 'ACF_PRO' ) ) {
-			$this->markTestSkipped( 'ACF Pro is not active so this test will not run.' );
-		}
-
 		parent::__construct();
 	}
 }
