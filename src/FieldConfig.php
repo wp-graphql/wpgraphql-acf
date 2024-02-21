4938913db24309ec5de4b5954645ffdcbcdb7c95
Jason Bahl <jasonbahl@mac.com>
2024-02-21 16:29:39 -0700

    - composer fix-cs - add translators: comments

--- src/FieldConfig.php
+++ src/FieldConfig.php
@@ -136,7 +136,7 @@ class FieldConfig {
 	public function get_field_description(): string {
 
 		$graphql_field_type = $this->get_graphql_field_type();
-		$field_type_config = ( $graphql_field_type instanceof AcfGraphQLFieldType ) ? $graphql_field_type->get_config() : [];
+		$field_type_config  = ( $graphql_field_type instanceof AcfGraphQLFieldType ) ? $graphql_field_type->get_config() : [];
 
 		// Use the explicit graphql_description, if set
 		if ( ! empty( $this->acf_field['graphql_description'] ) ) {
@@ -157,7 +157,6 @@ class FieldConfig {
 		}
 
 		if ( isset( $field_type_config['graphql_description_after'] ) ) {
-
 			if ( is_callable( $field_type_config['graphql_description_after'] ) ) {
 				$description .= ' ' . call_user_func( $field_type_config['graphql_description_after'], $this );
 			} else {
