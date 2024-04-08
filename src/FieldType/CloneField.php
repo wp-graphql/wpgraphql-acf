7a43e86e52fbe567899d94ed7545c4894c6f306a
Jason Bahl <jasonbahl@mac.com>
2024-04-08 16:06:01 -0600

    - update logic in the clone field to better handle prefixed and non-prefixed clone field groups

--- src/FieldType/CloneField.php
+++ src/FieldType/CloneField.php
@@ -3,6 +3,7 @@ namespace WPGraphQL\Acf\FieldType;
 
 use WPGraphQL\Acf\AcfGraphQLFieldType;
 use WPGraphQL\Acf\FieldConfig;
+use WPGraphQL\Utils\Utils;
 
 class CloneField {
 
@@ -14,6 +15,57 @@ class CloneField {
 			'clone',
 			[
 				'graphql_type' => static function ( FieldConfig $field_config, AcfGraphQLFieldType $acf_field_type ) {
+
+					$sub_field_group = $field_config->get_raw_acf_field();
+					$parent_type     = $field_config->get_parent_graphql_type_name( $sub_field_group );
+					$field_name      = $field_config->get_graphql_field_name();
+					$registry        = $field_config->get_registry();
+					$type_name       = Utils::format_type_name( $parent_type . ' ' . $field_name );
+					$prefix_name     = $sub_field_group['prefix_name'] ?? false;
+
+					// If the "Clone" field has not set a "prefix_name",
+					// return NULL to prevent registering a new type
+					// The cloned
+					if ( ! $prefix_name ) {
+						return 'NULL';
+					}
+
+					$cloned_fields = array_filter(
+						array_map(
+							static function ( $cloned ) {
+								return acf_get_field( $cloned );
+							},
+							$sub_field_group['clone']
+						)
+					);
+
+					$cloned_group_interfaces = array_filter(
+						array_map(
+							static function ( $cloned ) use ( $field_config ) {
+								$cloned_group = acf_get_field_group( $cloned );
+								if ( empty( $cloned_group ) ) {
+									return null;
+								}
+								return $field_config->get_registry()->get_field_group_graphql_type_name( $cloned_group ) . '_Fields';
+							},
+							$sub_field_group['clone']
+						)
+					);
+
+
+					if ( ! empty( $cloned_group_interfaces ) ) {
+						$type_name = self::register_prefixed_clone_field_type( $type_name, $sub_field_group, $cloned_fields, $field_config );
+						register_graphql_interfaces_to_types( $cloned_group_interfaces, [ $type_name ] );
+						return $type_name;
+					}
+
+
+					// If the "Clone" field has cloned individual fields
+					if ( ! empty( $cloned_fields ) ) {
+						return self::register_prefixed_clone_field_type( $type_name, $sub_field_group, $cloned_fields, $field_config );
+					}
+
+					// Bail by returning a NULL type
 					return 'NULL';
 				},
 				// The clone field adds its own settings field to display
